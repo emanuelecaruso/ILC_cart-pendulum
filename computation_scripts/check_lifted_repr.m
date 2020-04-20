299fa7b39a6a8e0875223359c9c07966dd67592b
emanuelecaruso <caruso.manu81@gmail.com>
2020-04-20 17:47:25 +0200

    lifted repr

--- computation_scripts/check_lifted_repr.m
+++ computation_scripts/check_lifted_repr.m
@@ -1,45 +1,55 @@
 syms x0;
+x0=sym('x0',[4,1]);
+x1=sym('x1',[4,1]);
+x2=sym('x2',[4,1]);
+x3=sym('x3',[4,1]);
+A0=sym('A0',[4,4]);
+A1=sym('A1',[4,4]);
+A2=sym('A2',[4,4]);
+A3=sym('A3',[4,4]);
+B0=sym('B0',[4,1]);
+B1=sym('B1',[4,1]);
+B2=sym('B2',[4,1]);
+B3=sym('B3',[4,1]);
 
-N=4;      %number of timesteps
+xHistory=[x0, x1, x2, x3];
+N=length(xHistory);      %number of timesteps
 
-%A=sym('A',[4,4,N]);
-%B=sym('B',[4,N]);
-%A(:,:,1)=A0;
-%A(:,:,2)=A1;
-%A(:,:,3)=A2;
-%A(:,:,4)=A3;
-%B(:,1)=B0;
-%B(:,2)=B1;
-%B(:,3)=B2;
-%B(:,4)=B3;
-
-A=sym('A',N);
-B=sym('B',N);
+A=sym('A',[4,4,N]);
+B=sym('B',[4,N]);
+A(:,:,1)=A0;
+A(:,:,2)=A1;
+A(:,:,3)=A2;
+A(:,:,4)=A3;
+B(:,1)=B0;
+B(:,2)=B1;
+B(:,3)=B2;
+B(:,4)=B3;
 
 %compute F
-F=sym('F',[N,N]);
+F=sym('F',[N*4,N]);
 for m=1:N
     for l=1:N
         if m<l-1
-            A_product=1;
-            for k=(m+1):(l-1)
-                A_product=A_product*A(k);
+            A_product=eye(4);
+            for k=(l-1):-1:(m+1)
+                A_product=A_product*A(:,:,k);
             end
-            F(l,m)=A_product*B(m);
+            F(l*4-3:l*4,m)=A_product*B(:,m);
         elseif m==l-1
-            F(l,m)=B(m);
+            F(l*4-3:l*4,m)=B(:,m);
         else
-            F(l,m)=0;
+            F(l*4-3:l*4,m)=zeros(4,1);
         end
     end
 end
 
 %compute d0
-d0=sym('d0',[N,1]);
+d0=sym('d0',[N*4,1]);
 for k=1:N
-    A_product=1;
-    for i=1:k-1
-        A_product=A_product*A(i);
+    A_product=eye(4);
+    for i=k-1:-1:1
+        A_product=A_product*A(:,:,i);
     end
-    d0(k)=A_product*x0;
+    d0(k*4-3:k*4,1)=A_product*x0;
 end
